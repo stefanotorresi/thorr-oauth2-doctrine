7abbf5f3a61c1006405da63c59c88706471fb686
Stefano Torresi <stefano@torresi.io>
2015-03-03 13:28:06 +0100

    mark usermapper test as incomplete

---
+++ tests/DataMapper/UserMapperAdapterTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace Thorr\OAuth2\Doctrine\Test\DataMapper;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class UserMapperAdapterTest extends TestCase
+{
+    public function testFindByCredential()
+    {
+        $this->markTestIncomplete();
+    }
+}
